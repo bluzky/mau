3ebe43707a2048e8e4b9c75381e3809c9523b604
Always blue <bluesky.1289@gmail.com>
2025-10-29 15:27:30 +0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mau.MixProject do
   def project do
     [
       app: :mau,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
